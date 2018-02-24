99693b60420efa7f9e578676f8368d72030de440
Valentin Ignatyev <valentjedi@gmail.com>
2018-02-25 00:00:01 +1000

    jinja will work for xss fine too

--- shoppy/app.py
+++ shoppy/app.py
@@ -1,15 +1,8 @@
-import os
-from string import Template
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 
 
 @app.route('/')
 def catalog():
-    print(TEMPLATE_DIR)
-    with open(f'{TEMPLATE_DIR}/catalog.html') as f:
-        template = Template(f.read())
-    return template.substitute(content='Sup XSS')
+    return render_template('catalog.jinja2', content='Sup XSS')
