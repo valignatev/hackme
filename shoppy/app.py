42667739369193e73df21efca495e1f8f18ecd32
Valentin Ignatyev <valentjedi@gmail.com>
2018-02-24 23:23:50 +1000

    wired templates, veeery vulnerable of course

--- shoppy/app.py
+++ shoppy/app.py
@@ -1,8 +1,15 @@
+import os
+from string import Template
+
 from flask import Flask
 
 app = Flask(__name__)
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 
 
 @app.route('/')
-def hello():
-    return '<title>fuckrkn</title>Sup XSS'
+def catalog():
+    print(TEMPLATE_DIR)
+    with open(f'{TEMPLATE_DIR}/catalog.html') as f:
+        template = Template(f.read())
+    return template.substitute(content='Sup XSS')
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,5 +4,5 @@ import pytest
 
 @pytest.mark.usefixtures('live_server')
 def test_google(selenium):
-    selenium.get(url_for('hello', _external=True))
-    assert 'fuckrkn' == selenium.title
+    selenium.get(url_for('catalog', _external=True))
+    assert 'Shoppy catalog' == selenium.title
