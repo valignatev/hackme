92249947381693deca93114a11294417a261ffe4
Valentin Ignatyev <valentjedi@gmail.com>
2018-02-26 23:04:20 +1000

    mega products search

--- shoppy/app.py
+++ shoppy/app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
+PRODUCTS = [
+    {'name': 'dildo'},
+    {'name': 'antirkn'},
+    {'name': 'nimbus 2000'},
+]
+
+
 @app.route('/')
 def catalog():
-    catalog = [
-        {'content': 1},
-        {'content': 2},
-        {'content': 3},
-    ]
-    return render_template('catalog.jinja2', catalog=catalog)
+    search = request.args.get('q', '')
+    products = PRODUCTS
+    if search:
+        products = [p for p in PRODUCTS if search in p['name']]
+    return render_template('catalog.jinja2', products=products, search=search)
