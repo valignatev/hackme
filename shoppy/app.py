3c2abc2fc902f788cf6344cb98f964831bdefc15
Valentin Ignatyev <valentjedi@gmail.com>
2018-02-27 00:51:47 +1000

    add product detail page

--- shoppy/app.py
+++ shoppy/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
 
 PRODUCTS = [
-    {'name': 'dildo'},
-    {'name': 'antirkn'},
-    {'name': 'nimbus 2000'},
+    {'name': 'dildo', 'id': 1},
+    {'name': 'antirkn', 'id': 2},
+    {'name': 'nimbus 2000', 'id': 3},
 ]
 
 
@@ -17,3 +18,12 @@ def catalog():
     if search:
         products = [p for p in PRODUCTS if search in p['name']]
     return render_template('catalog.jinja2', products=products, search=search)
+
+
+@app.route('/product/<int:product_id>')
+def product(product_id):
+    try:
+        product = [p for p in PRODUCTS if p['id'] == product_id][0]
+    except IndexError:
+        abort(404)
+    return render_template('product.jinja2', product=product)
