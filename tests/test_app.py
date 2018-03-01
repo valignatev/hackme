5ffa6424b5b535a38ead34300aa69d4203ac47f2
Valentin Ignatev <valentjedi@gmail.com>
2018-03-02 00:26:36 +1000

    a bit more sophisticated selenium test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,16 @@
 from flask import url_for
 import pytest
+from selenium.webdriver.common.keys import Keys
+
+
+@pytest.fixture
+def selenium(selenium):
+    selenium.implicitly_wait(10)
+    return selenium
 
 
 @pytest.mark.usefixtures('live_server')
 def test_catalog(selenium):
     selenium.get(url_for('catalog', _external=True))
-    assert 3 == len(selenium.find_elements_by_class_name('product'))
+    selenium.find_element_by_tag_name('input').send_keys('t', Keys.ENTER)
+    assert 2 == len(selenium.find_elements_by_class_name('product'))
