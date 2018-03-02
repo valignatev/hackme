9b616b16ed292d27e7a8d08bd9fb2ecc2bfb2f55
Valentin Ignatev <valentjedi@gmail.com>
2018-03-03 01:48:18 +1000

    failing test_no_xss

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,9 @@
 from flask import url_for
 import pytest
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 pytestmark = pytest.mark.usefixtures('live_server')
 
@@ -15,3 +18,13 @@ def test_catalog(selenium):
     selenium.get(url_for('catalog', _external=True))
     selenium.find_element_by_tag_name('input').send_keys('t', Keys.ENTER)
     assert 2 == len(selenium.find_elements_by_class_name('product'))
+
+
+def test_no_xss(selenium):
+    selenium.get(url_for('catalog', _external=True))
+    selenium.find_element_by_tag_name('input').send_keys(
+        '<script>alert()</script>',
+        Keys.ENTER,
+    )
+    with pytest.raises(TimeoutException):
+        WebDriverWait(selenium, 1).until(EC.alert_is_present())
